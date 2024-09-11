588c316780a1790079c1bcb942268f5e420c672a
FalseF <busohag@gmail.com>
2024-09-11 01:07:08 -0400

    CAMS initialize

---
+++ CryptoAssetManagementSystem/CryptoAssetManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CryptoAssetManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
